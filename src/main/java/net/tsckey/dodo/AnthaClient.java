6ac915feb0f9ac1ffadf9097c41f528db3847575
TSCKey <omaraed@hotmail.com>
2025-02-05 03:37:41 +0100

    Initial commit

---
+++ src/main/java/net/tsckey/dodo/AnthaClient.java
@@ -0,0 +1,10 @@
+package net.tsckey.dodo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnthaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
